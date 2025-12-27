d66916175cddac9603ca99cbbc079423993df19a
Infamous003 <arfathsyed0312@gmail.com>
2025-12-27 14:18:20 +0530

    feat: add functionality to connect to db in the lifespan event
    - Add wait_for_db that connects and pings to the DB with retries and delays
    - Add get_db that yields a database session which can then be injected in routes/services

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.core.config import settings
+from app.db.db import wait_for_db
+from app.core.logging import get_logger
 
-app = FastAPI(app_name=settings.APP_NAME)
+logger = get_logger(__name__)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    wait_for_db()
+    yield
+
+
+app = FastAPI(app_name=settings.APP_NAME, lifespan=lifespan)
 
 @app.get("/")
 async def read_root():
